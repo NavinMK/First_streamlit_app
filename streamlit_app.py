66d846f8ac51cacf3dc676ab1bd15f617a31dace
NavinMK <navin16@yahoo.com>
2023-01-02 18:59:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text("🦪🥪Avacado Toast")
 import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_indez("Fruit")
-streamlit.muktiselect("Pick:",list(my_fruit_list.index))
+streamlit.multiselect("Pick:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
