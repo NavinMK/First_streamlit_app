9cacbd65f314ab023760c5d0453203d27d511e21
NavinMK <navin16@yahoo.com>
2023-01-02 18:50:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🍝Omega 3 & Blueberry Oatmeal")
 streamlit.text("🦪🥪Avacado Toast")
+import pandas
+my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
