4a4bd69229e2dbf0bdde9df0139196e4bf9dd996
NavinMK <navin16@yahoo.com>
2023-01-02 19:00:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.text("🍝Omega 3 & Blueberry Oatmeal")
 streamlit.text("🦪🥪Avacado Toast")
 import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_indez("Fruit")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 streamlit.multiselect("Pick:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
