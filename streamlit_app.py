6be1c3bcfa608de226bc4caac396c20157fa100c
NavinMK <navin16@yahoo.com>
2023-01-02 17:42:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("🍝Omega 3 & Blueberry Oatmeal")
+streamlit.text("🦪🥪Avacado Toast")
