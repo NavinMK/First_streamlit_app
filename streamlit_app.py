77741d1f7188d7ab24dc145e7ea61721c3a7ec19
NavinMK <navin16@yahoo.com>
2023-01-05 15:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,6 @@ my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 my_fruit_list = my_fruit_list.set_index("Fruit")
 streamlit.multiselect("Pick:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+streamlit.header("Fruitvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
