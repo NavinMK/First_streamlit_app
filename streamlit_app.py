4bf4dea23c2f8500019b9459f34d12743004ca1e
NavinMK <navin16@yahoo.com>
2023-01-05 15:40:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ fruits_selected = streamlit.multiselect("Pick:",list(my_fruit_list.index),['Avoc
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruitvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "watermelon")
 fruitvice_jsonnormalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 streamlit.dataframe(fruitvice_jsonnormalized)
