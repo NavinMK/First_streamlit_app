a103cab843c95e3f0ceb39ec4143cf67e22b7b0b
NavinMK <navin16@yahoo.com>
2023-01-06 21:34:26 +0530

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import requests
 import snowflake.connector
+import pandas
+from urllib.error import URLError
 
 my_cnx= snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
@@ -12,19 +14,27 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🍝Omega 3 & Blueberry Oatmeal")
 streamlit.text("🦪🥪Avacado Toast")
-import pandas
+
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 fruits_selected = streamlit.multiselect("Pick:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruitvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information abour?','Kiwi')
-streamlit.write('The user entered',fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-fruitvice_jsonnormalized = pandas.json_normalize(fruityvice_response.json())
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information abour?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+        fruitvice_jsonnormalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruitvice_jsonnormalized)
+except URLError as e:
+    streamlit.error()
 #streamlit.text(fruityvice_response.json())
-streamlit.dataframe(fruitvice_jsonnormalized)
+
+
+streamlit.write('The user entered',fruit_choice)
 fruit_sfselect = streamlit.text_input("what fruit would you like to add?",'jackfruit')
 streamlit.text("Thanks for adding"+fruit_sfselect)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
